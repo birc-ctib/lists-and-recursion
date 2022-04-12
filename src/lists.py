cd3ea77aa8a53263989ef2bda7a18e7b3416767e
Thomas Mailund <mailund@birc.au.dk>
2022-04-12 08:17:44 +0200

    Removed solutions

--- src/lists.py
+++ src/lists.py
@@ -71,9 +71,7 @@ def contains(x: List[T], e: T) -> bool:
     >>> contains(L(1, L(2, L(3, None))), 2)
     True
     """
-    return False if x is None \
-        else True if x.head == e \
-        else contains(x.tail, e)
+    ...
 
 
 def drop(x: List[T], k: int) -> List[T]:
@@ -88,10 +86,7 @@ def drop(x: List[T], k: int) -> List[T]:
     >>> drop(x, 3)
     L(4, None)
     """
-    if k == 0:
-        return x
-    assert x is not None, "We can't recurse on an empty list"
-    return drop(x.tail, k - 1)
+    ...
 
 
 def keep(x: List[T], k: int) -> List[T]:
@@ -105,10 +100,7 @@ def keep(x: List[T], k: int) -> List[T]:
     >>> keep(x, 3)
     L(1, L(2, L(3, None)))
     """
-    if k == 0:
-        return None
-    assert x is not None, "Can't recurse on an empty list"
-    return L(x.head, keep(x.tail, k - 1))
+    ...
 
 
 def concat(x: List[T], y: List[T]) -> List[T]:
@@ -118,7 +110,7 @@ def concat(x: List[T], y: List[T]) -> List[T]:
     >>> concat(L(1, L(2, None)), L(3, L(4, None)))
     L(1, L(2, L(3, L(4, None))))
     """
-    return y if x is None else L(x.head, concat(x.tail, y))
+    ...
 
 
 def append(x: List[T], e: T) -> List[T]:
@@ -128,7 +120,7 @@ def append(x: List[T], e: T) -> List[T]:
     >>> append(L(1, L(2, None)), 3)
     L(1, L(2, L(3, None)))
     """
-    return concat(x, L(e, None))
+    ...
 
 
 def rev(x: List[T]) -> List[T]:
@@ -138,7 +130,7 @@ def rev(x: List[T]) -> List[T]:
     >>> rev(L(1, L(2, L(3, None))))
     L(3, L(2, L(1, None)))
     """
-    return None if x is None else append(rev(x.tail), x.head)
+    ...
 
 
 # Tail-recursive versions ###########################################
@@ -181,9 +173,7 @@ def contains_tr(x: List[T], e: T) -> bool:
     >>> contains_tr(L(1, L(2, L(3, None))), 2)
     True
     """
-    return False if x is None \
-        else True if x.head == e \
-        else contains_tr(x.tail, e)
+    ...
 
 
 def drop_tr(x: List[T], k: int) -> List[T]:
@@ -198,13 +188,10 @@ def drop_tr(x: List[T], k: int) -> List[T]:
     >>> drop_tr(x, 3)
     L(4, None)
     """
-    if k == 0:
-        return x
-    assert x is not None, "We can't recurse on an empty list"
-    return drop_tr(x.tail, k - 1)
+    ...
 
 
-def keep_tr(x: List[T], k: int, acc: List[T] = None) -> List[T]:
+def keep_tr(x: List[T], k: int) -> List[T]:
     """
     Keep only the first k elements.
 
@@ -215,31 +202,17 @@ def keep_tr(x: List[T], k: int, acc: List[T] = None) -> List[T]:
     >>> keep_tr(x, 3)
     L(1, L(2, L(3, None)))
     """
-    if k == 0:
-        return rev_tr(acc)
-    assert x is not None, "Can't recurse on an empty list"
-    return keep_tr(x.tail, k - 1, L(x.head, acc))
+    ...
 
 
-def rev_prepend(x: List[T], y: List[T]) -> List[T]:  # Helper function
-    """
-    Reverse and prepend x to y.
-
-    >>> rev_prepend(L(2, L(1, None)), L(3, L(4, None)))
-    L(1, L(2, L(3, L(4, None))))
-    """
-    return y if x is None else rev_prepend(x.tail, L(x.head, y))
-
-
-def concat_tr(x: List[T], y: List[T], acc: List[T] = None) -> List[T]:
+def concat_tr(x: List[T], y: List[T]) -> List[T]:
     """
     Concatenate x and y.
 
     >>> concat_tr(L(1, L(2, None)), L(3, L(4, None)))
     L(1, L(2, L(3, L(4, None))))
     """
-    return rev_prepend(acc, y) if x is None \
-        else concat_tr(x.tail, y, L(x.head, acc))
+    ...
 
 
 def append_tr(x: List[T], e: T) -> List[T]:
@@ -249,7 +222,7 @@ def append_tr(x: List[T], e: T) -> List[T]:
     >>> append_tr(L(1, L(2, None)), 3)
     L(1, L(2, L(3, None)))
     """
-    return concat_tr(x, L(e, None))
+    ...
 
 
 def rev_tr(x: List[T]) -> List[T]:
@@ -259,7 +232,7 @@ def rev_tr(x: List[T]) -> List[T]:
     >>> rev_tr(L(1, L(2, L(3, None))))
     L(3, L(2, L(1, None)))
     """
-    return rev_prepend(x, None)
+    ...
 
 
 # Loop versions ###########################################
@@ -309,11 +282,7 @@ def contains_loop(x: List[T], e: T) -> bool:
     >>> contains_loop(L(1, L(2, L(3, None))), 2)
     True
     """
-    while x:
-        if x.head == e:
-            return True
-        x = x.tail
-    return False
+    ...
 
 
 def drop_loop(x: List[T], k: int) -> List[T]:
@@ -328,10 +297,7 @@ def drop_loop(x: List[T], k: int) -> List[T]:
     >>> drop_loop(x, 3)
     L(4, None)
     """
-    for _ in range(k):
-        assert x is not None, "We can't recurse on an empty list"
-        x = x.tail
-    return x
+    ...
 
 
 def keep_loop(x: List[T], k: int) -> List[T]:
@@ -345,25 +311,7 @@ def keep_loop(x: List[T], k: int) -> List[T]:
     >>> keep_loop(x, 3)
     L(1, L(2, L(3, None)))
     """
-    acc = None
-    for _ in range(k):
-        assert x is not None, "Can't recurse on an empty list"
-        acc = L(x.head, acc)
-        x = x.tail
-    return rev_loop(acc)
-
-
-def rev_prepend_loop(x: List[T], y: List[T]) -> List[T]:  # Helper function
-    """
-    Reverse and prepend x to y.
-
-    >>> rev_prepend_loop(L(2, L(1, None)), L(3, L(4, None)))
-    L(1, L(2, L(3, L(4, None))))
-    """
-    while x:
-        y = L(x.head, y)
-        x = x.tail
-    return y
+    ...
 
 
 def concat_loop(x: List[T], y: List[T]) -> List[T]:
@@ -373,9 +321,7 @@ def concat_loop(x: List[T], y: List[T]) -> List[T]:
     >>> concat_loop(L(1, L(2, None)), L(3, L(4, None)))
     L(1, L(2, L(3, L(4, None))))
     """
-    # both rev_loop and rev_prepend_loop are looping, so we can use them
-    # without exceeding the stack limit
-    return rev_prepend_loop(rev_loop(x), y)
+    ...
 
 
 def append_loop(x: List[T], e: T) -> List[T]:
@@ -385,7 +331,7 @@ def append_loop(x: List[T], e: T) -> List[T]:
     >>> append_loop(L(1, L(2, None)), 3)
     L(1, L(2, L(3, None)))
     """
-    return concat_loop(x, L(e, None))
+    ...
 
 
 def rev_loop(x: List[T]) -> List[T]:
@@ -395,8 +341,4 @@ def rev_loop(x: List[T]) -> List[T]:
     >>> rev_loop(L(1, L(2, L(3, None))))
     L(3, L(2, L(1, None)))
     """
-    acc = None
-    while x:
-        acc = L(x.head, acc)
-        x = x.tail
-    return acc
+    ...
--- src/test_lists.py
+++ src/test_lists.py
@@ -2,5 +2,5 @@
 # all files that start with test_*.py and run all functions with
 # names that start with test_
 
-def test_1984():
-    assert 2 + 2 == 4
+def test_missing():
+    assert False, "If you want tests you have to write them"
